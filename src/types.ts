fe4564b9c2750a1964a83dd0b640dea8dbf05892
Dolmetscher <novtechtrans@gmail.com>
2025-09-23 21:29:22 +0300

    Update types.ts
    Enhanced game logic

--- src/types.ts
+++ src/types.ts
@@ -1,4 +1,14 @@
 
+export interface WordOption {
+  word: string;
+  emoji: string;
+}
+
+export interface LetterData {
+  letter: string;
+  choices: WordOption[];
+}
+
 export interface AlphabetItem {
   letter: string;
   word: string;
